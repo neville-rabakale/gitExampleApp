996c0891cf511815e440444aef5ce1a941a578ed
Neville Rabakale <neville.rabakale@gmail.com>
2022-11-28 14:42:12 +0100

    Add project files.

---
+++ gitExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gitExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
